92d04ddcc7508315f54cdf8999265b0cfef8f915
Markus Översti <markus@oversti.org>
2021-04-01 10:18:50 +0000

    Torstai turvaan, osa 2

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -1,17 +1,37 @@
 import React, { useState } from 'react'
 
-const App = () => {
+const App = (props) => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  
+  const setToZero = () => setCounter(0)
+
+  const Display = ({ counter }) => <div>{counter}</div>
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
   )
-
-  console.log('rendering...', counter)
-
+  
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />             
+    </div>
   )
 }
 
