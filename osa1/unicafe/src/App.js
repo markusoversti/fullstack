ced4e38516a6b0e7d21a53fcf53b4fd16e923f74
Markus Översti <markus@oversti.org>
2021-04-19 08:10:12 +0000

    Tilastot refaktoroitu

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Statistics = ({good, neutral, bad}) => {
+  return(
+    <div>
+  <h1>Statistics</h1>
+  Good: {good}<br/>
+  Neutral: {neutral}<br/>
+  Bad: {bad}<br/>
+  All: {good + neutral + bad}<br/>
+  Average: { (1*good + 0*neutral + (-1)*bad)/(good+neutral+bad) }<br/>
+  Positive: {good / (good+neutral+bad) * 100} %<br/>
+</div>
+  )
 }
 
-export default App;
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+const handleClick = () => console.log('Nappia painettiin!!!')
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <button onClick={() => setGood(good+1)}>
+        good
+      </button>
+
+      <button onClick={() => setNeutral(neutral+1)}>
+        neutral
+      </button>
+
+      <button onClick={() => setBad(bad+1)}>
+        bad
+      </button>
+
+
+    <br/>
+    <Statistics good={good} neutral={neutral} bad={bad}/>
+
+    </div>
+  )
+}
+
+export default App
